31042f2f72cac7dd90ed1ec3752a4d68f484fbd3
Samantha Kofler <samantha_kofler@outlook.com>
2020-08-21 10:06:20 +0300

    Updates

---
+++ precache-manifest.c80518c384eab3df00bd3de52abe97ac.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2c0ecb45c038598eeaa2e20a028fd910",
+    "url": "/Mr-Bitcoin-React/index.html"
+  },
+  {
+    "revision": "62d421dd915a08822f99",
+    "url": "/Mr-Bitcoin-React/static/css/main.0e2ec9e2.chunk.css"
+  },
+  {
+    "revision": "68325a191ed30c69cc4c",
+    "url": "/Mr-Bitcoin-React/static/js/2.e1779a6e.chunk.js"
+  },
+  {
+    "revision": "9b9d26b7e222c44d5f08f95405cf8214",
+    "url": "/Mr-Bitcoin-React/static/js/2.e1779a6e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "62d421dd915a08822f99",
+    "url": "/Mr-Bitcoin-React/static/js/main.de4a8648.chunk.js"
+  },
+  {
+    "revision": "1ae402774275a90178de",
+    "url": "/Mr-Bitcoin-React/static/js/runtime-main.ef20b4d8.js"
+  },
+  {
+    "revision": "754c8ffb0875d5d6a11e6b3c3bdd5d23",
+    "url": "/Mr-Bitcoin-React/static/media/Ranga-Regular.754c8ffb.ttf"
+  }
+]);
